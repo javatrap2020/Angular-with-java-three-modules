430195e237e3f8051272ace042fd2d71e10f7402
Oleh Boiko <javatrap2020@gmail.com>
2022-01-14 14:46:38 +0200

    add module backend

---
+++ backend/src/main/java/backend/springbootjavatrap2020/SpringBootJavatrap2020Application.java
@@ -0,0 +1,13 @@
+package backend.springbootjavatrap2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavatrap2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJavatrap2020Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/backend/springbootjavatrap2020/SpringBootJavatrap2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.springbootjavatrap2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavatrap2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
