cfad642da41627f20f560d4e138b02d331091613
Oleh Boiko <javatrap2020@gmail.com>
2022-01-16 21:30:19 +0200

    add links-backend

---
+++ backend/src/main/java/backend/springbootjavatrap2020/links/UserLinks.java
@@ -0,0 +1,9 @@
+package backend.springbootjavatrap2020.links;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserLinks {
+    public static final String LIST_USERS = "/users";
+    public static final String ADD_USER = "/user";
+}
